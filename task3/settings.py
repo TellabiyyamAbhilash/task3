a4a2a7211ce6bbb55be182bf72431eb4c9e9171a
Abhi_19 <109691577+TellabiyyamAbhilash@users.noreply.github.com>
2023-05-12 14:13:31 +0530

    changes

--- task3/settings.py
+++ task3/settings.py
@@ -80,10 +80,8 @@ WSGI_APPLICATION = 'task3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sweet',
-        'USER': 'sweetuser',
-        'PASSWORD': 'sweet123',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- task3/urls.py
+++ task3/urls.py
@@ -14,17 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from accounts import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
+
 
 urlpatterns = [
     path('',views.homepage),
     path('admin/', admin.site.urls),
     path('acc/',include('accounts.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
